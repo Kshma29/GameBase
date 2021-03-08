2614aa091032620694a640a9f4ba5bd2998f3b54
Kshma Singh <77921246+Kshma29@users.noreply.github.com>
2021-03-08 23:46:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+function login() {
+    var txtEmail = document.getElementById('exampleInputEmail1').value;
+    var txtPassword = document.getElementById('exampleInputPassword1').value;
+    window.alert(txtEmail+" "+txtPassword);
+}
