3bd597926285b2ebb110fa0815718886c2320c19
Kshma Singh <77921246+Kshma29@users.noreply.github.com>
2021-04-07 23:20:48 +0530

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-function login() {
-    var txtEmail = document.getElementById('exampleInputEmail1').value;
-    var txtPassword = document.getElementById('exampleInputPassword1').value;
-    window.alert(txtEmail+" "+txtPassword);
-}
